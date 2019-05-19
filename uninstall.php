f70616e367440c4ad2edb26ec1b0bf7dc4123481
Stefano Zurlini <s.zurlini@gmail.com>
2019-05-19 16:24:33 +0200

    Nuove modifiche

--- uninstall.php
+++ uninstall.php
@@ -2,29 +2,3 @@
 /*
  * Uninstall plugin
  */
-	
-if ( is_multisite() ) {
-	$ms_sites = function_exists( 'get_sites' ) ? get_sites() : wp_get_sites();
-
-	if( 0 < sizeof( $ms_sites ) ) {
-		foreach ( $ms_sites as $ms_site ) {
-			$blog_id = isset( $ms_site['blog_id'] ) ? $ms_site['blog_id'] : $ms_site->blog_id;
-			switch_to_blog( $blog_id );
-			if( sizeof( $option_names ) > 0 ) {
-				foreach( $option_names as $option_name ) {
-					delete_option( $option_name );
-					plugin_uninstalled();
-				}
-			}
-		}
-	}
-
-	restore_current_blog();
-} else {
-	if( sizeof( $option_names ) > 0 ) {
-		foreach( $option_names as $option_name ) {
-			delete_option( $option_name );
-			plugin_uninstalled();
-		}
-	}
-}
